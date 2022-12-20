69081553f2478e4f1356c2249070563232fe45af
willcfyang2 <356730059@qq.com>
2022-12-20 06:18:21 -0500

    http request to same server in angular

--- src/app/biz.service.ts
+++ src/app/biz.service.ts
@@ -8,31 +8,33 @@ import { BizInfo, GetBizListRsp} from "./biz_info";
 })
 export class BizService {
 
+  // host = 'https://flask-server-angular.azurewebsites.net';
+  host = '';
   constructor(
     private http: HttpClient,
   ) { }
   getBizList() {
-        return this.http.get<GetBizListRsp>('http://192.168.5.23:5002/get_biz_list', {
+        return this.http.get<GetBizListRsp>('/get_biz_list', {
             responseType: 'json'
         }); 
     }
   getBizInfo(bizId: string) {
-        return this.http.get('http://192.168.5.23:5002/get_biz_info', {
+        return this.http.get('/get_biz_info', {
             responseType: 'json',
             params: {id: bizId}
         }); 
     }
   deleteBizInfo(bizId: string) {
-        return this.http.get('http://192.168.5.23:5002/delete_biz_info', {
+        return this.http.get('/delete_biz_info', {
             responseType: 'json',
             params: {id: bizId}
         }); 
     }
   addCompanyInfo(info: Object) {
-        return this.http.post('http://192.168.5.23:5002/add_company_info', info); 
+        return this.http.post('/add_company_info', info); 
     }
   updateCompanyInfo(info: Object) {
         // info['id'] = bizId;
-        return this.http.post('http://192.168.5.23:5002/update_company_info', info); 
+        return this.http.post('/update_company_info', info); 
     }
 }
